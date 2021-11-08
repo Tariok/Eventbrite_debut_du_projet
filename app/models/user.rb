701494c42350e6710f53805146e8f5d258cb0a38
Tariok <mingotevan@gmail.com>
2021-11-08 18:35:39 +0100

    3emepush

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
   has_many :attendances
   has_many :events, through: :attendances
   has_many :events, foreign_key: 'event_id', class_name: 'Event'
+
+  after_create :welcome_send
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 end
